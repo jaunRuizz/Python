cd2d01f4f41b1e44460130828f9fcca1702297b8
jaunRuizz <daniel.luevanoui@uanl.edu.mx>
2021-09-13 18:52:48 -0500

    Script en python para Scanear puertos de una IP Victima

---
+++ PORT-SCAN.py
@@ -0,0 +1,46 @@
+# danruizLSTI
+# Librerias importadas Nmap
+import nmap
+print()
+print("  _______    ________     ____      ____  ")
+print(" |       \  /   _    \   /    \    /    \ ")
+print(" |  |-|   ||   | |    | |      \__/     | ")
+print(" |  |-|   ||   |_|    | |   |\     /|   | ")
+print(" |_______/  \________/  |___| \___/ |___| ")
+print()
+
+
+print("[Info] Herramienta Para Escnear los puertos abiertos de un host")
+print(" || Escrito totalmente en python usando libreria Nmap")
+print(" || Creador de la Herramienta DanLSTI")
+print()
+
+
+def scan():
+    # Se manda llamar el modulo portscan y se fuarda en la variable nm
+    nm = nmap.PortScanner()
+    # Se le solicita la ip al usuario
+    ip = input("[-] Ingresa la ip a scanear: --> ")
+    # Se comienza a scanear la ip ingresada
+    scaner = nm.scan(ip)
+    count = 0
+    puertos_abiertos = "-p"
+    print("Host :  %s" % ip)
+    print("Stado : %s" % nm[ip].state())
+    # ciclo para listar todos los puertos de todos los protocolos UDP o TCP
+    for protocolo in nm[ip].all_protocols():
+        print("Protocolo : %s" % protocolo)
+        lport = nm[ip][protocolo].keys()
+        sorted(lport)
+        for port in lport:
+            print("Puerto : %s\tstate : %s" %
+                  (port, nm[ip][protocolo][port]["state"]))
+            if count == 0:
+                puertos_abiertos = puertos_abiertos+" "+str(port)
+                count = 1
+            else:
+                puertos_abiertos = puertos_abiertos+","+str(port)
+    print("Purtos abiertos: "+puertos_abiertos+" " + "--> "+str(ip))
+
+
+scan()
