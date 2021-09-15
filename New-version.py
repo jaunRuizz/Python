3ad1ca7bdb675f2f43031b05ba288f3dd868fce5
jaunRuizz <daniel.luevanoui@uanl.edu.mx>
2021-09-14 19:17:56 -0500

    Nueva version PORT-SCAN con barra de carga

--- New-version.py
+++ New-version.py
@@ -1,5 +1,7 @@
 # danruizLSTI
 # Librerias importadas Nmap
+import sys 
+import  argparse
 import nmap
 from progress.bar import Bar, ChargingBar
 import os
@@ -21,41 +23,45 @@ print()
 
 
 def barra():
-    bar2 = ChargingBar('Instalando:', max=100)
+    bar2 = ChargingBar('Escanenado:', max=100)
     for num in range(100):
-        time.sleep(random.uniform(0, 0.2))
+        time.sleep(random.uniform(0, 0.1))
         bar2.next()
     bar2.finish()
 
 
-def scan():
+def scan(): 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ip", dest="ip", help="Ingresa la ip de la victima")
+    ip = parser.parse_args()
     # Se manda llamar el modulo portscan y se fuarda en la variable nm
     nm = nmap.PortScanner()
     # Se le solicita la ip al usuario
-    ip = input("[-] Ingresa la ip a scanear: --> ")
+    # ip = input("[-] Ingresa la ip a scanear: --> ")
     # Se comienza a scanear la ip ingresada
-    scaner = nm.scan(ip)
+    scaner = nm.scan(ip.ip)
+    barra()
     count = 0
     puertos_abiertos = "-p"
-    print("Host :  %s" % ip)
-    print("Stado : %s" % nm[ip].state())
+    print("Host :  %s" % ip.ip)
+    print("Stado : %s" % nm[ip.ip].state())
     # ciclo para listar todos los puertos de todos los protocolos UDP o TCP
-    for protocolo in nm[ip].all_protocols():
+    for protocolo in nm[ip.ip].all_protocols():
         print("Protocolo : %s" % protocolo)
-        lport = nm[ip][protocolo].keys()
+        lport = nm[ip.ip][protocolo].keys()
         sorted(lport)
         for port in lport:
             print("Puerto : %s\tstate : %s" %
-                  (port, nm[ip][protocolo][port]["state"]))
+                  (port, nm[ip.ip][protocolo][port]["state"]))
             if count == 0:
                 puertos_abiertos = puertos_abiertos+" "+str(port)
                 count = 1
             else:
                 puertos_abiertos = puertos_abiertos+","+str(port)
-    print("Purtos abiertos: "+puertos_abiertos+" " + "--> "+str(ip))
+    print("Purtos abiertos: "+puertos_abiertos+" " + "--> "+str(ip.ip))
 
 
 if __name__ == "__main__":
-    barra()
+    print("Iniciando Scaneo")
     scan()
     
